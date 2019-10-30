d93970cc590b177486fc7e515bba466d0dd5905d
bluejeans117 <vishnurajesh45@gmail.com>
2019-10-30 12:07:50 +0530

    rebase php scripts to newer version
    Compatible with Linux now

--- login.php
+++ login.php
@@ -1,48 +1,94 @@
-<?php
-include 'db.php';
-session_start();
-$errmsg_arr = array();
-$errflag = false;
-// configuration
-// new data
-
-$user = $_POST['email'];
-$password = $_POST['password'];
-
-
-
-// query
-$result = $conn->prepare("SELECT * FROM customers WHERE email= :hjhjhjh AND password= :asas");
-$result->bindParam(':hjhjhjh', $user);
-$result->bindParam(':asas', $password);
-$result->execute();
-
-
-if($result) {
-
-$_SESSION['email'] = $user;
-$_SESSION['password'] = $password;
-
-$result = $conn->prepare("SELECT * FROM customers WHERE email= :hjhjhjh");
-$result->bindParam(':hjhjhjh', $user);
-
-$result->execute();
-$rows = $result->fetch();
-$_SESSION['name'] = $rows['name'];
-$_SESSION['id']=  $rows['cid'];
-
-	header("location: home.php");
+<?php include 'inc/header.php'; ?>
+<?php 
+$login = Session::get("cuslogin");
+if ($login == true) {
+    header("Location:order.php");
 }
-else {
-	header("location: loginfail.php");
+ ?>
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+    $custLogin = $cmr->customerLogin($_POST);
 }
-
-
-if($errflag) {
-	$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-	session_write_close();
-	header("location: home.php");
-	exit();
-}
-
 ?>
+ <div class="main">
+    <div class="content">
+    	 <div class="login_panel">
+    	 	<?php 
+            if (isset($custLogin)) {
+                echo $custLogin;
+            }
+             ?>
+        	<h3>Existing Customers</h3>
+        	<p>Sign in with the form below.</p>
+        	<form action="" method="post">
+                	<input name="email" placeholder="Email" type="text"/>
+                    <input name="pass" placeholder="Password" type="password"/>
+                 
+                 <p class="note">If you forgot your passoword just enter your email and click <a href="#">here</a></p>
+                    <div class="buttons"><div><button class="grey" name="login">Sign In</button></div></div>
+                    </div>
+                    </form>
+                    <?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
+    $customerReg = $cmr->customerRegistration($_POST);
+}
+?>
+    	<div class="register_account">
+    		<?php 
+            if (isset($customerReg)) {
+                echo $customerReg;
+            }
+             ?>
+    		<h3>Register New Account</h3>
+    		<form action="" method="post">
+		   			 <table>
+		   				<tbody>
+						<tr>
+						<td>
+							<div>
+							<input type="text" name="name" placeholder="Name"/>
+							</div>
+							<div>
+							<input type="text" name="address" placeholder="Address"/>
+							</div>
+							
+							<div>
+							   <input type="text" name="city" placeholder="City"/>
+							</div>
+							
+							<div>
+								<input type="text" name="zip" placeholder="Zip Code"/>
+							</div>
+							
+		    			 </td>
+		    			<td>
+		    				<div>
+								<input type="text" name="email" placeholder="Email"/>
+							</div>
+						<div>
+							<input type="text" name="country" placeholder="Country"/>
+						</div>
+		    			        
+	
+		           <div>
+		          <input type="text" name="phone" placeholder="Phone"/>
+		          </div>
+				  
+				  <div>
+					<input type="text" name="pass" placeholder="Password"/>
+				</div>
+		    	</td>
+		    </tr> 
+		    </tbody></table> 
+		   <div class="search"><div><button class="grey" name="register">Create Account</button></div></div>
+		    <p class="terms">By clicking 'Create Account' you agree to the <a href="#">Terms &amp; Conditions</a>.</p>
+		    <div class="clear"></div>
+		    </form>
+    	</div>  	
+       <div class="clear"></div>
+    </div>
+ </div>
+<?php include 'inc/footer.php'; ?>
+
--- payment.php
+++ payment.php
@@ -1,112 +1,28 @@
-<?php
-include 'db.php';
- session_start();
- include 'redirect.php';
-
-$cat = $_REQUEST['sum'];
-
-$r = $conn->prepare("SELECT * from card where customerid = ?");
-$r->bindParam(1,$_SESSION['id']);
-$r->execute();
-
-if($r)
-{
-  $rr = $conn->prepare("SELECT * from card where customerid = ?");
-  $rr->bindParam(1,$_SESSION['id']);
-  $rr->execute();
-$rt = $rr->fetch();
-  $name = $rt['name'];
-  $card = $rt['cardno'];
-  $expiry = $rt['expiry'];
-  $cvv = $rt['cvv'];
+<?php include 'inc/header.php'; ?>
+<?php 
+$login = Session::get("cuslogin");
+if ($login == false) {
+    header("Location:login.php");
 }
-else {
-  $name = " ";
-  $card = " ";
-  $expiry = " ";
-  $cvv = " ";
-
-}
-?>
-
-<!DOCTYPE html>
-<html >
-<head>
-  <meta charset="UTF-8">
-  <title>Payment Form</title>
-
-
-  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
-
-      <link rel="stylesheet" href="style.css">
-
-
-</head>
-
-<body>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-<div class="container">
-  <div id="Checkout" class="inline">
-      <h1>Pay Invoice</h1>
-      <div class="card-row">
-          <span class="visa"></span>
-          <span class="mastercard"></span>
-          <span class="amex"></span>
-          <span class="discover"></span>
-      </div>
-      <form method="post" action="paymentdone.php?sum=<?php echo $cat; ?>">
-          <div class="form-group">
-              <label for="PaymentAmount">Payment amount</label>
-              <div class="amount-placeholder">
-                  <span>INR</span>
-                  <span><?php echo $cat; ?></span>
-              </div>
-          </div>
-          <div class="form-group">
-              <label or="NameOnCard">Name on card</label>
-              <input id="NameOnCard" name="name" class="form-control" type="text" value="<?php echo $name; ?>"></input>
-          </div>
-          <div class="form-group">
-              <label for="CreditCardNumber">Card number</label>
-              <input id="CreditCardNumber" name="cardno" class="null card-image form-control" type="text" value="<?php echo $card; ?>"></input>
-          </div>
-          <div class="expiry-date-group form-group">
-              <label for="ExpiryDate">Expiry date</label>
-              <input id="ExpiryDate" class="form-control" name="expiry" type="text" value="<?php echo $expiry; ?>"></input>
-          </div>
-          <div class="security-code-group form-group">
-              <label for="SecurityCode">Security code</label>
-              <div class="input-container" >
-                  <input id="SecurityCode" name="cvv" class="form-control" type="text" value="<?php echo $cvv; ?>"></input>
-                  <i id="cvc" class="fa fa-question-circle"></i>
-              </div>
-              <div class="cvc-preview-container two-card hide">
-                  <div class="amex-cvc-preview"></div>
-                  <div class="visa-mc-dis-cvc-preview"></div>
-              </div>
-          </div>
-          <button id="PayButton" class="btn btn-block btn-success submit-button" type="submit">
-              <span class="submit-button-lock"></span>
-              <span class="align-middle">Pay <?php echo $cat; ?></span>
-          </button>
-      </form>
-      <form action="codform.php?sum=<?php echo $cat; ?>" method="post ">
-        <div class="form-group">
-        <!-- <a href="codform.php?sum=<?php echo $cat; ?>"> -->
-          <button id="PayB" class="btn btn-block btn-info submit-button" type="submit">
-            <span class="submit-button-home"></span>
-            <span class="align-middle">Cash On Delivery</span>
-          </button>
-        <!-- </a> -->
-        </div>
-      </form>
-      </div>
-    </div>
-  </div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
-  <script src="index.js"></script>
-</body>
-</html>
+ ?>
+<style type="text/css">
+.payment{width: 500px; min-height: 200px; text-align: center; border: 1px solid #ddd; margin: 0 auto; padding:50px;}	
+.payment h2{border-bottom: 1px solid #ddd; margin-bottom: 40px; padding-bottom: 10px;}	
+.payment a{background: #3C3B40; border-radius: 3px; color:#fff; font-size:22px; padding:5px 30px;}
+.back a{width:150px; margin: 5px auto 0; padding: 7px 0; text-align: center; display: block; background: #555; border:1px solid #333; color: #fff; border-radius: 3px; font-size: 25px; }	
+</style>
+ 
+<div class="main">
+	<div class="content">
+		<div class="section group">
+		<div class="payment">
+			<h2>Choose Payment Ontion</h2>
+			<a href="paymentoffiline.php">Cash On Delivery</a>
+			<a href="paymentonline.php">Online Payment</a>
+		</div>
+		<div class="back">
+			<a href="cart.php">Previous</a>
+		</div>				
+		</div>
+	</div>
+<?php include 'inc/footer.php'; ?>
