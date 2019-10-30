628aa9bc2b28a0122f421ca9793422de20964ee7
bluejeans117 <vishnurajesh45@gmail.com>
2019-10-30 09:54:19 +0530

    PURGE TF OUT OF EVERYTHING

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$con =  mysqli_connect("localhost","username","password","DBMS");
-	if(mysqli_connect_errno()){
-		echo "Error occurred while connecting with Database".mysqli_connect_errno();
-	}
-	
-?>
--- search.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include("connect.php");
-	$result = $_POST['Search'];
-	
-	$query = mysqli_query($con,"SELECT * FROM PRODUCT WHERE Category = '$result'");
-	
-?>
